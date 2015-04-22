d09355b41f409e3cd88fc81d186bd2b002ac8915
Jeff Brunson <jeff.a.brunson@gmail.com>
2015-04-22 09:19:05 -0600

    rails json api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
  root to: 'notes#index'
 
- resources :notes, only: [:index, :show]
+ resources :notes, only: [:index, :create, :update, :destroy]
+
+ get '*any' => 'notes#index' #if anyone types route, it will go to index page and still use BB routes
 end
